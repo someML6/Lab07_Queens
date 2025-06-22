40fcb240a5b74710244bb395dffd7dadfa2cbc2b
joshu <jmao2500@gmail.com>
2025-06-22 14:14:08 +0200

    Added Board Class

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+
+    public static void main(String[] args) {
+         Board b = new Board();
+
+
+         System.out.print(b.getBoard()[2][3] );
+
+
+    }
+}
